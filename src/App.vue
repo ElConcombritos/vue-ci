7f1eaf77a986d90e53f87e4f3bbc586e3d580b55
ElConcombritos <alexis.carru8@gmail.com>
2022-03-28 16:36:56 +0200

    first step todo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Todo/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Todo from "@/components/Todo";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Todo
   }
 }
 </script>
